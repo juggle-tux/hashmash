035a2bea7989901e22d161c7dabad64ca5b8ac0c
Sven Steinbauer <sven@unlogic.co.uk>
2017-04-13 16:11:25 +0100

    Using rayon

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::thread;
+#![allow(non_camel_case_types)]
+
 use std::io::Read;
 use std::fs::File;
 use std::error::Error;
@@ -8,10 +9,8 @@ use crypto::{sha1, sha2, md5};
 //use crypto::buffer::{ReadBuffer, WriteBuffer, BufferResult};
 use crypto::digest::Digest;
 
-extern crate num_cpus;
-
-extern crate scoped_threadpool;
-use scoped_threadpool::Pool;
+extern crate rayon;
+use rayon::prelude::*;
 
 extern crate permutohedron;
 use permutohedron::Heap;
@@ -21,6 +20,7 @@ extern crate clap;
 use clap::{Arg, App};
 
 arg_enum!{
+    #[derive(Clone, Copy)]
     enum Hashes {
         md5,
         sha1,
@@ -31,22 +31,8 @@ arg_enum!{
     }
 }
 
-arg_enum!{
-    #[derive(Debug)]
-    enum Delims {
-        none,
-        colon,
-        space,
-        ampersand,
-        comma,
-        period,
-        hyphen,
-        underscore,
-        pipe,
-        semicolon
-    }
-}
-
+// Generate a hash from the input based on the requested hash type
+// Less duplicated code needed here
 fn generate_hash(hash: &Hashes, data: &str) -> String {
     match hash {
         &Hashes::md5 => { let mut hasher = md5::Md5::new(); hasher.input_str(data); hasher.result_str() },
@@ -58,10 +44,17 @@ fn generate_hash(hash: &Hashes, data: &str) -> String {
     }
 }
 
+// Read lines from file into a Vec<String>
 fn read_file(filename: &str) -> Result<Vec<String>, Box<Error>> {
     let mut buffer = String::new();
-    let mut f = try!(File::open(filename));
-    try!(f.read_to_string(&mut buffer));
+    let mut f = match File::open(filename) {
+        Ok(f) => f,
+        Err(why) => panic!("Unable to open file {}: {}", filename, why.description())
+    };
+    match f.read_to_string(&mut buffer) {
+        Ok(x) => x,
+        Err(why) => panic!("Unable to read from file: {}", why.description())
+    };
 
     let x = buffer
             .lines()
@@ -89,8 +82,8 @@ fn main() {
                                 .value_name("delim")
                                 .help("Select delimter")
                                 .takes_value(true)
-                                .default_value("none")
-                                .possible_values(&Delims::variants()))
+                                .default_value("")
+                                .possible_values(&[":", " ", "&", ",", ".", ".", "-", "_", "|", ";", ""]))
                           .arg(Arg::with_name("match")
                                 .short("m")
                                 .long("match")
@@ -108,47 +101,20 @@ fn main() {
                                .get_matches();
 
     let hash_alg = value_t!(matches.value_of("alg"), Hashes).unwrap();
-    let delimeter = value_t!(matches.value_of("delim"), Delims).unwrap();
+    let delimeter = value_t_or_exit!(matches.value_of("delim"), String);
     let filename = value_t_or_exit!(matches.value_of("file"), String);
     let match_hash = value_t_or_exit!(matches.value_of("match"), String);
 
-    let thread_num = num_cpus::get();
-
-    /*let mut perms = Vec::new();
-
-    let mut pool = Pool::new(thread_num);
-    pool.scoped(|scope| {
-        // Create references to each element in the vector ...
-        for e in &mut vec {
-            // ... and add 1 to it in a seperate thread
-            scope.execute(move || {
-                *e += 1;
-            });
-        }
-    });*/
-    println! ("Hash alg is {} {} {} {}", hash_alg, delimeter, filename, match_hash);
+    let delim = &delimeter[..];
 
     let mut file_data = read_file(&filename[..]).unwrap();
 
-    let mut heap = Heap::new(&mut file_data);
-    while let Some(elt) = heap.next_permutation() {
-        let res = generate_hash(&hash_alg, &elt.join("-"));
-        println!("{:?} {}", elt, res);
-        if res == match_hash {
-            println! ("Matching {}", &elt.join("-"));
-            break;
-        }
+    let heap: Vec<Vec <String>> = Heap::new(&mut file_data).collect();
 
+    let r = heap.par_iter().find_first(|&x| { generate_hash(&hash_alg, &(x.join(delim))) == match_hash });
+    if r.is_some() {
+        println!("[+] Found a matching hash for: {}", r.unwrap().join(delim));
+    } else {
+        println!("[+] Search exhausted. Nothing found");
     }
-
-    /*'outer: for e in &file_data {
-        'inner: for f in &file_data {
-            let res = generate_hash(&hash_alg, &format!("{}{}", e, f)[..]);
-            println!("{}", res);
-            if res == match_hash {
-                println!("{} {}", e, f);
-                break 'outer;
-            }
-        }
-    }*/
 }
